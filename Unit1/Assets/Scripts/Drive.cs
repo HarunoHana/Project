a3c454a929f2e0909a6ac1868198fbef0d3525aa
HarunoHana <jtcho1234@gmail.com>
2026-01-24 21:56:22 -0500

    week3

--- Unit1/Assets/Scripts/Drive.cs
+++ Unit1/Assets/Scripts/Drive.cs
@@ -2,16 +2,27 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
+    float forwardSpeed = 10.0f;
+    float rotateSpeed = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(Vector3.forward * Time.deltaTime);
+        float forwardInput = Input.GetAxis("Vertical");
+        forwardInput = forwardInput * forwardSpeed;
+
+        this.transform.Translate(Vector3.forward * Time.deltaTime * forwardInput);
+
+        float rotateInput = Input.GetAxis("Horizontal");
+        rotateInput = rotateInput * rotateSpeed;
+        rotateInput *= rotateSpeed;
+        rotateInput *= Time.deltaTime;
+        this.transform.Rotate(0, rotateInput, 0);
 
     }
 }
