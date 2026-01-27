207639433da94dbb318c2409fa45d57e0c089f8d
HarunoHana <jtcho1234@gmail.com>
2026-01-27 00:01:02 -0500

    w3c1

--- IntroToVR_SolarSystem/Assets/Scrips/Move.cs
+++ IntroToVR_SolarSystem/Assets/Scrips/Move.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    float speed = 10.0f;
+    float rotSpeed = 80.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,7 +13,15 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float forwardInput = Input.GetAxis("Vertical");
+        forwardInput *= speed;
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput *= rotSpeed;
+        horizontalInput *= Time.deltaTime;
+
         //this.transform.Translate(Vector3.forward);
-        this.transform.Translate(Vector3.forward * Time.deltaTime);
+        this.transform.Translate(Vector3.forward * Time.deltaTime * forwardInput);
+        this.transform.Rotate(0,horizontalInput, 0);
     }
 }
